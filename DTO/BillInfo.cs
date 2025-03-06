ce467ee54c2152a4d9082404bb65849b35456eab
tgiang04 <119372224+tgiang04@users.noreply.github.com>
2025-03-06 16:33:19 +0000

    Dong goi du lieu doi tuong BillInfo

---
+++ DTO/BillInfo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QLCF_APP.DAO;
+
+namespace QLCF_APP.DTO
+{
+    public class BillInfo
+    {
+        public BillInfo(int iD, int billID, int foodID, int count)
+        {
+            this.ID = iD;
+            this.BillID = billID;
+            this.FoodID = foodID;
+            this.Count = count;
+        }
+
+        public BillInfo(DataRow row) 
+        {
+            this.ID = (int)row["iD"];
+            this.BillID = (int)row["idBill"];
+            this.FoodID = (int)row["idFood"];
+            this.Count = (int)row["count"];
+        }
+
+        private int iD;
+
+        private int billID;
+
+        private int foodID;
+
+        private int count;
+
+        public int ID { get => iD; set => iD = value; }
+        public int BillID { get => billID; set => billID = value; }
+        public int FoodID { get => foodID; set => foodID = value; }
+        public int Count { get => count; set => count = value; }
+    }
+}
