d1ed464746dd7cd909a28f69d91d572412dd2e1e
tgiang04 <119372224+tgiang04@users.noreply.github.com>
2025-03-06 16:27:46 +0000

    Phuong thuc cho doi tuong Account

---
+++ DAO/AccountDAO.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QLCF_APP.DTO;
+
+namespace QLCF_APP.DAO
+{
+    internal class AccountDAO
+    {
+        private static AccountDAO instance;
+
+        public static AccountDAO Instance
+        {
+            get { if (instance == null) instance = new AccountDAO(); return AccountDAO.instance; }
+            private set { AccountDAO.instance = value; }
+        }
+
+        private AccountDAO() { }
+
+        public bool login(string username, string password)
+        {
+            string query = "USP_Login @userName , @passWord";
+            DataTable result = DataProvider.Instance.ExecuteQuery(query, new object[] { username, password });
+            return result.Rows.Count > 0;
+        }
+
+        public bool UpdateAccount(string userName, string displayName ,string password, string newpassword)
+        {
+            int result = DataProvider.Instance.ExecuteNonQuery("exec USP_UpdateAccount @userName , @displayName , @password , @newPassword", new object[] { userName, displayName ,password, newpassword });
+            return result > 0;
+        }
+
+        public Account GetAccountByUserName(string userName)
+        {
+            DataTable data = DataProvider.Instance.ExecuteQuery("Select * from Account where userName = '" + userName + "'");
+            foreach (DataRow item in data.Rows)
+            {
+                return new Account(item);
+            }
+            return null;
+        }
+
+        public DataTable GetListAccount()
+        {
+            return DataProvider.Instance.ExecuteQuery("Select userName, displayName, type from Account");
+        }
+
+        public bool InsertAccount(string userName, string displayName, int type)
+        {
+            string query = string.Format("INSERT dbo.Account (userName, displayName, type) VALUES ( N'{0}', N'{1}', {2})", userName, displayName, type);
+            int result = DataProvider.Instance.ExecuteNonQuery(query);
+            return result > 0;
+        }
+
+        public bool UpdateAccount(string userName, string displayName, int type)
+        {
+            string query = string.Format("UPDATE dbo.Account SET displayName = N'{1}', type = {2} WHERE userName = N'{0}'", userName, displayName, type);
+            int result = DataProvider.Instance.ExecuteNonQuery(query);
+            return result > 0;
+        }
+
+        public bool DeleteAccount(string userName)
+        {
+            
+
+            string query = string.Format("Delete Account where userName = N'{0}'", userName);
+            int result = DataProvider.Instance.ExecuteNonQuery(query);
+            return result > 0;
+        }
+
+        
+    }
+}
