06dbf6e13a50e2a6a3ce8b9940f1be72bd38cf43
tgiang04 <119372224+tgiang04@users.noreply.github.com>
2025-03-06 16:34:00 +0000

    Dong goi du lieu doi tuong Menu

---
+++ DTO/Menu.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLCF_APP.DTO
+{
+    public class Menu
+    {
+        public Menu(string name, int count, float price, float totalPrice = 0)
+        {
+            this.FoodName = name;
+            this.Count = count;
+            this.Price = price;
+            this.TotalPrice = totalPrice;
+        }
+
+        public Menu(DataRow row)
+        {
+            this.FoodName = row["name"].ToString();
+            this.Count = (int)row["count"];
+            this.Price = (float)Convert.ToDouble(row["price"].ToString());
+            this.TotalPrice = (float)Convert.ToDouble(row["totalPrice"].ToString());
+        }
+
+        private string name;
+
+        private int count;
+
+        private float price;
+
+        private float totalPrice;
+
+        public string FoodName { get => name; set => name = value; }
+        public int Count { get => count; set => count = value; }
+        public float Price { get => price; set => price = value; }
+        public float TotalPrice { get => totalPrice; set => totalPrice = value; }
+    }
+}
