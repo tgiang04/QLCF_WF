5a340889388fc0606d0e8c2a5552294991f48c13
tgiang04 <119372224+tgiang04@users.noreply.github.com>
2025-03-06 16:36:23 +0000

    Layout Login

---
+++ Service/loginForm.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using QLCF_APP.DAO;
+using QLCF_APP.DTO;
+
+namespace QLCF_APP
+{
+    public partial class loginForm : Form
+    {
+        public loginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void loginForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string userName = this.txbUserName.Text;
+            string passWord = this.txbPassWord.Text;
+            if ( login(userName, passWord) )
+            {
+                Account loginAccount = AccountDAO.Instance.GetAccountByUserName(userName);
+                tableManager T = new tableManager(loginAccount);
+                this.Hide();
+                T.ShowDialog();
+                this.Show();
+            }
+            else 
+            {
+                MessageBox.Show("Sai tài khoản hoặc mật khẩu");    
+            }
+        }
+
+        public bool login(string userName, string passWord)
+        {
+            //byte[] temp = ASCIIEncoding.ASCII.GetBytes(passWord);
+            //byte[] hasData = new MD5CryptoServiceProvider().ComputeHash(temp);
+
+            //string hasPass = "";
+
+            //foreach (byte item in hasData)
+            //{
+            //    hasPass += item;
+            //}
+            
+
+            string query = "USP_Login @userName , @password";
+            DataTable result = DataProvider.Instance.ExecuteQuery(query, new object[] { userName, passWord });
+            return result.Rows.Count > 0;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void loginForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Bạn có thực sự muốn thoát chương trình", "Thông báo" ,MessageBoxButtons.OKCancel) != System.Windows.Forms.DialogResult.OK)
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
